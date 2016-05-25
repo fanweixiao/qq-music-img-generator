7c316b35563c18e692df86fb8e6a5b358d543c58
fanweixiao <fanweixiao@gmail.com>
2016-05-25 19:05:47 +0800

    Update package.json

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-var _supportedArtistImageSizes = [58, 68, 90, 126, 150, 180, 300, 500, 660, 750];
-var _supportedAlbumImageSizes  = [58, 68, 90, 126, 150, 180, 300, 500];
+var _supportedArtistImageSizes = [58, 68, 90, 126, 150, 180, 300, 500, 660, 750]
+var _supportedAlbumImageSizes = [58, 68, 90, 126, 150, 180, 300, 500]
 
-var _artist_url_pattern = "http://i.gtimg.cn/music/photo/mid_singer_$size$/$ch2$/$ch1$/$ID$.jpg";
-var _album_url_pattern  = "http://i.gtimg.cn/music/photo/mid_album_$size$/$ch2$/$ch1$/$ID$.jpg";
+var _artist_url_pattern = 'http://i.gtimg.cn/music/photo/mid_singer_$size$/$ch2$/$ch1$/$ID$.jpg'
+var _album_url_pattern = 'http://i.gtimg.cn/music/photo/mid_album_$size$/$ch2$/$ch1$/$ID$.jpg'
 
-function validateSize(arr, _size){
-  if(arr.indexOf(_size) >= 0){
-    return _size;
+function validateSize (arr, _size) {
+  if (arr.indexOf(_size) >= 0) {
+    return _size
   }
   // if _size larger than largest one, return the largest one
-  var _larget = arr[arr.length -1];
-  if(_larget < _size){
-    return _larget;
+  var _larget = arr[arr.length - 1]
+  if (_larget < _size) {
+    return _larget
   }
   // find the first one bigger than _size in _supportedSizes
-  for(var i = 0, len = arr.length; i < len; i++){
-    if(arr[i] > _size){
-      _size = arr[i];
-      break;
+  for (var i = 0, len = arr.length; i < len; i++) {
+    if (arr[i] > _size) {
+      _size = arr[i]
+      break
     }
   }
-  return _size;
-};
+  return _size
+}
 
-function makeImageUrl(url_pattern, size_arr, id, size){
-  if(!id){
-    throw new Error('id must be specified');
+function makeImageUrl (url_pattern, size_arr, id, size) {
+  if (!id) {
+    throw new Error('id must be specified')
   }
   // default size is 300
-  if(typeof size === 'undefined'){
-    size = size || 300;
+  if (typeof size === 'undefined') {
+    size = size || 300
   } else {
-    size = validateSize(size_arr, size);
+    size = validateSize(size_arr, size)
   }
   // Album:
   //   [input]  001KH6924JPQ7a
@@ -39,20 +39,20 @@ function makeImageUrl(url_pattern, size_arr, id, size){
   // Artist:
   //   [input]
   //   [output] http://i.gtimg.cn/music/photo/mid_singer_500/8/T/002vbc3A2bv88T.jpg
-  var ch1 = id[id.length -1];
-  var ch2 = id[id.length -2];
+  var ch1 = id[id.length - 1]
+  var ch2 = id[id.length - 2]
 
   var url = url_pattern.replace('$size$', size)
     .replace('$ch2$', ch2)
     .replace('$ch1$', ch1)
-    .replace('$ID$', id);
-  return url;
-};
+    .replace('$ID$', id)
+  return url
+}
 
-exports.makeArtistImageUrl = function makeArtistImageUrl(artistID, size){
-  return makeImageUrl(_artist_url_pattern, _supportedArtistImageSizes, artistID, size);
-};
+exports.makeArtistImageUrl = function makeArtistImageUrl (artistID, size) {
+  return makeImageUrl(_artist_url_pattern, _supportedArtistImageSizes, artistID, size)
+}
 
-exports.makeAlbumImageUrl = function makeAlbumImageUrl(albumID, size){
-  return makeImageUrl(_album_url_pattern, _supportedAlbumImageSizes, albumID, size);
-};
+exports.makeAlbumImageUrl = function makeAlbumImageUrl (albumID, size) {
+  return makeImageUrl(_album_url_pattern, _supportedAlbumImageSizes, albumID, size)
+}
--- test/index.js
+++ test/index.js
@@ -1,61 +1,61 @@
-var assert = require('assert');
+var assert = require('assert')
 
-var gnr = require('../index');
+var gnr = require('../index')
 
 describe('Artist avatar test', function(){
   describe('size=300, even if size is not specified', function(){
     it('should get 300px, even if size not specified', function(){
       'http://i.gtimg.cn/music/photo/mid_singer_300/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeArtistImageUrl('001KH6924JPQ7a', 300));
       'http://i.gtimg.cn/music/photo/mid_singer_300/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeArtistImageUrl('001KH6924JPQ7a'));
-    });
-  });
+    })
+  })
   describe('size=500', function(){
     it('should get 500px', function(){
       'http://i.gtimg.cn/music/photo/mid_singer_500/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeArtistImageUrl('001KH6924JPQ7a', 500));
-    });
-  });
+    })
+  })
   describe('size=57', function(){
     it('should get 58px', function(){
       'http://i.gtimg.cn/music/photo/mid_singer_58/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeArtistImageUrl('001KH6924JPQ7a', 57));
-    });
-  });
+    })
+  })
   describe('size=59', function(){
     it('should get 68px', function(){
       'http://i.gtimg.cn/music/photo/mid_singer_68/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeArtistImageUrl('001KH6924JPQ7a', 59));
-    });
-  });
+    })
+  })
   describe('size=751', function(){
     it('should get 750px', function(){
       'http://i.gtimg.cn/music/photo/mid_singer_750/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeArtistImageUrl('001KH6924JPQ7a', 751));
-    });
-  });
-});
+    })
+  })
+})
 
 describe('Album image test', function(){
   describe('size=300, even if size is not specified', function(){
     it('should get 300px, even if size not specified', function(){
       'http://i.gtimg.cn/music/photo/mid_album_300/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeAlbumImageUrl('001KH6924JPQ7a', 300));
       'http://i.gtimg.cn/music/photo/mid_album_300/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeAlbumImageUrl('001KH6924JPQ7a'));
-    });
-  });
+    })
+  })
   describe('size=500', function(){
     it('should get 500px', function(){
       'http://i.gtimg.cn/music/photo/mid_album_500/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeAlbumImageUrl('001KH6924JPQ7a', 500));
-    });
-  });
+    })
+  })
   describe('size=57', function(){
     it('should get 58px', function(){
       'http://i.gtimg.cn/music/photo/mid_album_58/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeAlbumImageUrl('001KH6924JPQ7a', 57));
-    });
-  });
+    })
+  })
   describe('size=59', function(){
     it('should get 68px', function(){
       'http://i.gtimg.cn/music/photo/mid_album_68/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeAlbumImageUrl('001KH6924JPQ7a', 59));
-    });
-  });
+    })
+  })
   describe('size=751', function(){
     it('should get 500px', function(){
       'http://i.gtimg.cn/music/photo/mid_album_500/7/a/001KH6924JPQ7a.jpg'.should.equal( gnr.makeAlbumImageUrl('001KH6924JPQ7a', 751));
-    });
-  });
-});
+    })
+  })
+})
